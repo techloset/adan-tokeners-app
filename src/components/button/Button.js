c6a507b5114d2f4169443fae0f7c84c17f3c7b9d
passbooklet <passbooklets@gmail.com>
2023-08-28 13:32:58 +0500

    Shadow modified

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -4,7 +4,7 @@ const Button = () => {
   return (
 
     <div className="flex justify-center items-center h-[48px] w-[147px] cursor-pointer mt-5">
-      <div className="bg-green-600 rounded-3xl  shadow-green-500 shadow-lg ">
+      <div className="bg-green-600 rounded-3xl   shadow-xl shadow-green-800/50">
         <div className="text-white text-center py-2 px-6">
           Learn more
         </div>
